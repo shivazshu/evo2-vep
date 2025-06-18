74bc27fc80d15b4cb530d2dcac6a43a4e8a90b7e
Shivanshu Singh <shivanshusingh@Mac.alphionsee.in>
2025-06-18 23:15:12 +0530

    Update frontend configuration files

--- evo2-frontend/next.config.js
+++ evo2-frontend/next.config.js
@@ -2,17 +2,19 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import "./src/env.js";
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false, 
     output: 'export',
-};
-
-export default nextConfig;
+    distDir: 'out',
+    images: {
+      unoptimized: true
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    }
+  }
+  
+  export default nextConfig
